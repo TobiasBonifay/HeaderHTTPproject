49373dabc59f73da77bac84ed6ce20b426f6b764
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2023-04-14 08:54:43 +0200

    extract js

---
+++ HeaderHTTPproject/wwwroot/loader.js
@@ -0,0 +1,29 @@
+﻿const form = document.getElementById('analyze-form');
+const resultDiv = document.getElementById('result');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    resultDiv.innerHTML = 'Loading';
+
+    let dots = 1;
+    const interval = setInterval(() => {
+        resultDiv.innerHTML = `Loading${'.'.repeat(dots)}`;
+        dots = (dots + 1) % 4;
+    }, 500);
+
+    try {
+        const response = await fetch(event.target.action, {
+            method: event.target.method,
+            body: formData
+        });
+        resultDiv.innerHTML = await response.text();
+    } catch (e) {
+        console.error(e);
+        resultDiv.innerHTML = '<p>An error occurred while loading the result.</p>';
+    }
+
+    clearInterval(interval);
+});
